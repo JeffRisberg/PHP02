7744098b14194d0758d36d16f9ac29755c1083dd
Brandon Risberg <brandon.risberg@gmail.com>
2014-10-14 23:41:59 -0700

    recorded uscitp deloyment credentials
    Added delpoyment comments.

--- _connect.php
+++ _connect.php
@@ -1,5 +1,12 @@
 <?php
 
+// usctip.com/cpanel deployment credentials
+//$user = "risberg_php02";
+//$password = "~hrRwp{mPeNP";
+//$host = "uscitp.com";
+//$db_name = "risberg_php02";
+
+// localhost testing credentials
 $user = "developer";
 $password = "123456";
 $host = "localhost";
