7ed2947bb47cfe2a86887d1c536d92fa29a7c9d0
Brandon Risberg <brandon.risberg@gmail.com>
2014-10-29 16:51:52 -0700

    Fixed up the update action
    Fixed the update action to correctly update the order record

--- order_edit.php
+++ order_edit.php
@@ -28,23 +28,35 @@ if (!$results = mysqli_query($db_connection, $sql)) {
 echo "Number of rows returned: " . mysqli_num_rows($results) . "<br>";
 echo "Order_id=" . $order_id . "<br>";
 
-$row = $results->fetch_row();
+$row = $results->fetch_assoc();
 //var_dump($row);
+
+$sql = <<<SQL
+SELECT *
+FROM pizza_types
+SQL;
+
+
+if (!$pizza_result = mysqli_query($db_connection, $sql)) {
+    die('There was an error running the query [' . mysqli_error($db_connection) . ']');
+}
+
 ?>
 
-<form action="order_place.php">
+<form action="order_update.php">
+    <input type="hidden" name="order_id" value="<?php echo $order_id; ?>" />
     <table>
         <tr>
             <td>My name:</td>
-            <td><input type="text" name="buyer_name" value="<?php echo $row["buyer_id"]; ?>" disabled></td>
+            <td><input type="text" name="user_id" value="<?php echo $row["user_id"]; ?>" readonly></td>
         </tr>
         <tr>
             <td>My type:</td>
             <td>
                 <select name="type_id">
                     <?php
-                    while ($row = $result->fetch_assoc()) {
-                        echo "<option value='" . $row['id'] . "'>" . $row['name'] . "</option>";
+                    while ($pizza_type_row = $pizza_result->fetch_assoc()) {
+                        echo "<option value='" . $pizza_type_row['id'] . "'>" . $pizza_type_row['name'] . "</option>";
                     }
                     ?>
                 </select>
--- order_update.php
+++ order_update.php
@@ -1,3 +1,5 @@
+<?php include '_connect.php'; ?>
+
 <?php
 /**
  * Created by PhpStorm.
@@ -6,21 +8,16 @@
  * Time: 10:50 PM
  */
 
+var_dump($_REQUEST);
+
 $order_id=$_REQUEST['order_id'];
-$buyer_name = $_REQUEST['buyer_name'];
-$type_name = $_REQUEST['type_name'];
-
-$user = "developer";
-$password = "123456";
-$host = "localhost";
-$db_name = "php01";
-
-$db_connection = mysqli_connect($host, $user, $password, $db_name);
+$user_id = $_REQUEST['user_id'];
+$type_id = $_REQUEST['type_id'];
 
     $sql = <<<SQL
 UPDATE orders
-SET buyer_name='$buyer_name', type_name='$type_name', last_updated=now()
-WHERE order_id=$order_id;
+SET user_id='$user_id', type_id='$type_id', last_updated=now()
+WHERE id=$order_id;
 SQL;
 
 if (!$result = mysqli_query($db_connection, $sql)) {
